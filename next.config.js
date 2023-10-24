d0408cc84452c56c916fa53b12442a6162935074
zirkelc <chris.zirkel@gmail.com>
2023-10-24 07:28:27 +0200

    update redirect

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/nodes',
+        source: '/nodes-2023',
         destination: 'https://zirkelc.notion.site/zirkelc/NODES-2023-e5538f884e4749f8aea3f3ad24f2a207',
         permanent: true,
       },
